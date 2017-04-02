16e1ac226a962770473e7a5a51241cac21ae1982
jumblecube <jumblecubelab@gmail.com>
2017-04-02 00:33:14 -0700

    Added additional

--- mistimage.py
+++ mistimage.py
@@ -31,3 +31,9 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                     y_: mnist.test.labels}))
+prediction = tf.argmax(y, 1)
+print "predictions", prediction.eval(feed_dict={x: mnist.test.images},
+                                     session=sess)
+probabilities = y
+print "probabilities", probabilities.eval(feed_dict={x: mnist.test.images},
+                                          session=sess)
